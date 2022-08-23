ce5f12bdc3737ead628ef747c0caef3772961971
Sören Link <S.Link@porta.de>
2022-08-23 12:16:59 +0200

    fix(animation): fix double quote prettier issue

--- Moorhuhn/src/objectChicken.js
+++ Moorhuhn/src/objectChicken.js
@@ -1,4 +1,4 @@
-import { getRandomCoordinates } from "../util/randomCoords";
+import { getRandomCoordinates } from '../util/randomCoords';
 import {
   currentScoreUserName,
   arenaB,
@@ -6,8 +6,8 @@ import {
   arenaX,
   arenaY,
   scoreDiv,
-} from "../main";
-import { getScore, userNameInput } from "./scoreboard";
+} from '../main';
+import { getScore, userNameInput } from './scoreboard';
 
 let chickenCounter = 0;
 
@@ -24,8 +24,8 @@ class Chicken {
 
   moveChicken() {
     const timeout = this.getRandomTime();
-    const xCoords = getRandomCoordinates(arenaX, arenaR) + "px";
-    const yCoords = getRandomCoordinates(arenaY, arenaB) + "px";
+    const xCoords = getRandomCoordinates(arenaX, arenaR) + 'px';
+    const yCoords = getRandomCoordinates(arenaY, arenaB) + 'px';
     this.chicken.style = `top: ${yCoords}; left: ${xCoords}; transition: ${timeout};`;
 
     this.timer = setTimeout(() => {
@@ -34,48 +34,48 @@ class Chicken {
   }
 
   createChicken() {
-    const chickenEl = document.createElement("img");
+    const chickenEl = document.createElement('img');
     chickenEl.setAttribute(
-      "src",
-      "https://cdn.imgbin.com/10/3/20/imgbin-dead-man-s-hand-moorhuhn-the-settlers-7-paths-to-a-kingdom-computer-icons-xiii-others-54JUqkV2eLFasFMnRnP9rSDi1.jpg"
+      'src',
+      'https://cdn.imgbin.com/10/3/20/imgbin-dead-man-s-hand-moorhuhn-the-settlers-7-paths-to-a-kingdom-computer-icons-xiii-others-54JUqkV2eLFasFMnRnP9rSDi1.jpg'
     );
-    chickenEl.style.left = getRandomCoordinates(arenaX, arenaR) + "px";
-    chickenEl.style.top = getRandomCoordinates(arenaY, arenaB) + "px";
-    chickenEl.classList.add("chicken");
+    chickenEl.style.left = getRandomCoordinates(arenaX, arenaR) + 'px';
+    chickenEl.style.top = getRandomCoordinates(arenaY, arenaB) + 'px';
+    chickenEl.classList.add('chicken');
     insertChickenArea.appendChild(chickenEl);
     return chickenEl;
   }
 
   clickChicken(chickenElement) {
-    console.log("click");
+    console.log('click');
     console.log(this.chicken);
     insertChickenArea.removeChild(chickenElement);
     currentScoreUserName.currentScore++;
     getScore();
     chickenCounter--;
     if (chickenCounter === 0) {
-      const bdDiv = document.createElement("div");
-      bdDiv.classList.toggle("backdrop");
+      const bdDiv = document.createElement('div');
+      bdDiv.classList.toggle('backdrop');
 
-      const bdDivH = document.createElement("h2");
+      const bdDivH = document.createElement('h2');
       bdDivH.textContent = `Game won, you've shot ${currentScoreUserName.currentScore} chicken`;
 
-      const bdDivBtn = document.createElement("button");
-      bdDivBtn.textContent = "OK";
-      bdDivBtn.addEventListener("click", () => {
-        bdDiv.classList.toggle("backdrop");
+      const bdDivBtn = document.createElement('button');
+      bdDivBtn.textContent = 'OK';
+      bdDivBtn.addEventListener('click', () => {
+        bdDiv.classList.toggle('backdrop');
         document.body.removeChild(bdDiv);
         console.log(chickenCounter);
-        const newScoreEntry = document.createElement("p");
-        if (currentScoreUserName.userName === "") {
-          currentScoreUserName.userName = "DUMMY";
+        const newScoreEntry = document.createElement('p');
+        if (currentScoreUserName.userName === '') {
+          currentScoreUserName.userName = 'DUMMY';
           newScoreEntry.textContent = `${currentScoreUserName.userName}: ${currentScoreUserName.currentScore}`;
         } else {
           newScoreEntry.textContent = `${currentScoreUserName.userName}: ${currentScoreUserName.currentScore}`;
         }
         scoreDiv.appendChild(newScoreEntry);
         currentScoreUserName.userName = userNameInput.value;
-        score.innerHTML = "";
+        score.innerHTML = '';
       });
 
       document.body.appendChild(bdDiv);
@@ -90,11 +90,11 @@ export const initChicken = (amount) => {
   let chickens = [];
   for (let i = 1; i <= amount; i++) {
     // create dom element instead
-    let newChicken = new Chicken("newInstanceOfChicken");
+    let newChicken = new Chicken('newInstanceOfChicken');
     chickens.push(newChicken);
     const chickenElement = newChicken.chicken;
     console.log(chickenElement);
-    chickenElement.addEventListener("click", () => {
+    chickenElement.addEventListener('click', () => {
       newChicken.clickChicken(chickenElement);
     });
     newChicken.moveChicken();
